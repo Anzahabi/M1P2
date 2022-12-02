7b44ba3beee8494ace8cc2619dacf839d455f117
Anzahabi <81551395+Anzahabi@users.noreply.github.com>
2022-12-02 15:51:15 +0700

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -6,25 +6,15 @@ st.title('Prediction Churn of Customer')
 
 # Create Submit Form
 with st.form(key='form_parameters'):
-    g = st.sidebar.selectbox(label='Gender', options=['Female', 'Male'])
-    s = st.sidebar.selectbox(label='SeniorCitizen', options=[0,1])
-    p = st.sidebar.selectbox(label='Partner', options=['No', 'Yes'])
-    d = st.sidebar.selectbox(label='Dependents', options=['No', 'Yes'])
-    t = st.number_input('Tenure', min_value=0, step=1, max_value=73)
-    ps = st.sidebar.selectbox(label='PhoneService', options=['No', 'Yes'])
-    ml = st.sidebar.selectbox(label='MultipleLines', options=['No phone service','No','Yes'])
-    ins = st.sidebar.selectbox(label='InternetService', options=['No','DSL','Fiber optic'])
-    ons = st.sidebar.selectbox(label='OnlineSecurity', options=['No internet service','No','Yes'])
-    onb = st.sidebar.selectbox(label='OnlineBackup', options=['No internet service','No','Yes'])
-    dp = st.sidebar.selectbox(label='DeviceProtection', options=['No internet service','No','Yes'])
-    ts = st.sidebar.selectbox(label='TechSupport', options=['No internet service','No','Yes'])
-    stv = st.sidebar.selectbox(label='StreamingTV', options=['No internet service','No','Yes'])
-    sm = st.sidebar.selectbox(label='StreamingMovies', options=['No internet service','No','Yes'])
-    con = st.sidebar.selectbox(label='Contract', options=['Month-to-month','One year','Two year'])
-    pb = st.sidebar.selectbox(label='PaperlessBilling', options=['No', 'Yes'])
-    pm = st.sidebar.selectbox(label='PaymentMethod', options=['Electronic check','Mailed check','Bank transfer (automatic)','Credit card (automatic)'])
-    mc = st.number_input('MonthlyCharges', min_value=18.25, step=0.05,max_value=118.75)
+  
+      
+    
+    
     tc = st.number_input('TotalCharges', min_value=18.8, step=0.02,max_value=8684.8)
+    mc = st.number_input('MonthlyCharges', min_value=18.25, step=0.05,max_value=118.75)
+    con = st.sidebar.selectbox(label='Contract', options=['Month-to-month','One year','Two year'])
+    pm = st.sidebar.selectbox(label='PaymentMethod', options=['Electronic check','Mailed check','Bank transfer (automatic)','Credit card (automatic)'])
+    ins = st.sidebar.selectbox(label='InternetService', options=['No','DSL','Fiber optic'])
 
     submitted = st.form_submit_button('Predict')
 
